b0c24408d05542333cc9e9d91a787bffe8db2d5f
Denis Mishankov <mishankov@main.com>
2024-12-25 11:59:07 +0300

    add proxy for dev server

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -2,5 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/meminfo': {
+				target: "http://localhost:4442",
+				ws: true
+			},
+			'/cpuinfo': {
+				target: "http://localhost:4442",
+				ws: true
+			},
+		}
+	}
 });
