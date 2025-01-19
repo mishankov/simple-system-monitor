3cb7759dd50d01d1ad86ad444510eae74a7459a8
Denis Mishankov <mishankov@mail.com>
2025-01-19 21:49:43 +0300

    add  cpu load service test

---
+++ internal/service/cpuinfo_test.go
@@ -0,0 +1,41 @@
+package service_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/mishankov/testman/assert"
+
+	"github.com/mishankov/simple-system-monitor/internal/domain/cpuinfo"
+	"github.com/mishankov/simple-system-monitor/internal/service"
+)
+
+func TestStreamCPULoad(t *testing.T) {
+	us := service.NewCPUInfoService(FakeCPULoadRepo{}, 1)
+	ctx, cancel := context.WithCancel(context.Background())
+	ch := make(chan []cpuinfo.CPULoad)
+
+	go us.StreamCPULoad(ctx, ch)
+
+	ci := <-ch
+	if assert.Equal(t, len(ci), 1) {
+		assert.Equal(t, ci[0].ID, "cpu123")
+	}
+
+	ci = <-ch
+	if assert.Equal(t, len(ci), 1) {
+		assert.Equal(t, ci[0].ID, "cpu123")
+	}
+
+	cancel()
+
+	_, ok := <-ch
+
+	assert.Equal(t, ok, false)
+}
+
+type FakeCPULoadRepo struct{}
+
+func (fcr FakeCPULoadRepo) GetCPUInfo() ([]cpuinfo.CPUInfo, error) {
+	return []cpuinfo.CPUInfo{{ID: "cpu123"}}, nil
+}
